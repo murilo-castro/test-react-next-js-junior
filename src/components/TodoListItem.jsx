45d8bddc96dd292ac78afbac3e8f6ad714346c43
Murilo Castro <murilo.65@gmail.com>
2023-07-26 07:58:39 -0300

    Resolvendo problemas com dependencias

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -3,7 +3,10 @@ import Link from 'next/link';
 const TodoListItem = ({ todo }) => {
   return (
     <div className="flex gap-5 justify-between items-center min-h-[86px] p-5">
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        className="flex gap-5 justify-between items-center min-h-[86px] p-5 accent-place"
+      />
       <li>{todo.title}</li>
       <Link href={`/update-task/${todo.id}`}>
         <button className={`${todo.done ? 'text-white' : 'text-black'}`}>
